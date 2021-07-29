094147c6074ae6c173a9c9bc06901d9fb265c202
Subhadeep Paul <subhadeeppaul35@gmail.com>
2021-07-29 08:17:28 +0000

    fix indentation error

--- app.py
+++ app.py
@@ -187,7 +187,7 @@ def main():
             text_newALL = prepCloud(text,Topic)
             wordcloud = WordCloud(stopwords=stopwords,max_words=800,max_font_size=70).generate(text_newALL)
             st.write(plt.imshow(wordcloud, interpolation='bilinear'))
-			plt.axis("off")
+            plt.axis("off")
             st.pyplot()
         
         
@@ -200,7 +200,7 @@ def main():
             #text_positive=" ".join([word for word in text_positive.split() if word not in stopwords])
             wordcloud = WordCloud(stopwords=stopwords,max_words=800,max_font_size=70).generate(text_new_positive)
             st.write(plt.imshow(wordcloud, interpolation='bilinear'))
-	    plt.axis("off")
+            plt.axis("off")
             st.pyplot()
         
         
